b2d2806e42a70281c5728893431764f4d455c66c
TomZe1982 <thomas.zeisner@gmail.com>
2021-09-20 09:48:11 +0200

    clean some imports

--- backend/src/main/java/de/tomze/backend/controller/BlogController.java
+++ backend/src/main/java/de/tomze/backend/controller/BlogController.java
@@ -11,9 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
-import java.util.Optional;
+
 
 import static org.springframework.http.ResponseEntity.ok;
 
